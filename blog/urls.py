30f46c72907be5a1b81e8f6ad0ce4efab20e134a
ramone74 <denis.apanovich@mail.ru>
2020-11-20 20:09:32 +0300

    ...

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-	path('', views.index, name = 'index'),
-	path('test/', views.test, name = 'test'),
+	path('', views.post_list, name='post_list'),
+	path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+	views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-def index(request):
-	return HttpResponse("Welcome to the club buddy!")
+def post_list(request):
+	#Обработчик для отображения списка статей
+	posts = Post.objects.all()
+	return render(request, 'blog/post/list.html', {'posts': posts})
 
-def test(request):
-	return HttpResponse("ПРОВЕРКА")
+def post_detail(request, year, month, day, post):
+	#Обработчик страницы статьи
+	post = get_object_or_404(Post, slug=post, status='published',publish__year=year,
+		publish__month=month,publish__day=day)
+	return render(request,'blog/post/detail.html',{'post': post})
