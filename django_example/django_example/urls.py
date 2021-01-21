c4f39a5eaa979d3dc11b25df28e4ced256cc80bb
ramone74 <74093066+ramone74@users.noreply.github.com>
2021-01-21 15:13:25 +0300

    Delete urls.py

--- django_example/django_example/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^riddles/', include('riddles.urls'), name="riddles"),
-    url(r'^admin/', admin.site.urls),
-]
